b8b5a37fd303ae5e8b7025aff8444b0cff40c29e
Artur Abdurakhmanov <arthursae@mail.ru>
2023-02-26 22:47:48 +0500

    use of values()

--- task21.py
+++ task21.py
@@ -3,8 +3,7 @@
 
 some_dict = {1:2, 3:4, 2:2}
 some_list = []
-for (k, v) in some_dict.items():
-    print(k, v)
+for v in some_dict.values():
     some_list.append(v)
 some_set = set(some_list)
 print(some_dict, '->', *set(some_set))
